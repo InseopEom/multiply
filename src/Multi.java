b637096b96ae2ee65db9877d9e3a52d8df203a0e
인섭 <@192.168.35.163>
2019-04-09 02:03:39 +0900

    구구단 오류

--- src/Multi.java
+++ src/Multi.java
@@ -6,10 +6,18 @@ public class Multi {
 		Scanner lo= new Scanner(System.in);
 		System.out.println("숫자 입력이dl요");
 		int num =lo.nextInt();
-		
-		for(int i=0;i<=9;i++) {
-			System.out.println(num+"*"+i+"="+num*i);
+		if(num<2) {
+			System.out.println("숫자가 작음");	
 		}
+		else if(num>9) {
+			System.out.println("숫자가 큼");
+		}
+		else {
+			for(int i=0;i<=9;i++) {
+				System.out.println(num+"*"+i+"="+num*i);
+			}
+		}
+		
 	}
 	
 
